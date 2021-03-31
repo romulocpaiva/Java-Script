c251c54a64909084364576b834c6165c994f2fd9
romulocpaiva <64547945+romulocpaiva@users.noreply.github.com>
2021-03-31 18:01:10 -0300

    Do while

---
+++ Maratona/Do while.js
@@ -0,0 +1,15 @@
+let a = 0;
+do {
+    console.log("Imprimindo de 0 a 4 !", a)
+    a++;
+}
+    while (a < 5);
+
+let b = 1;
+do {
+    console.log("Imprimindo de 1 a 5 !", b)
+    b ++;
+}
+    while (b <=5);
+
+
