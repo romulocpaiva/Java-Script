16b91bf5052a8829f30dd31f7bc7b7c1a3307b28
romulocpaiva <64547945+romulocpaiva@users.noreply.github.com>
2021-03-31 17:08:30 -0300

    for

---
+++ Maratona/for.js
@@ -0,0 +1,26 @@
+// For
+// while
+// do while
+// for in
+// for of
+
+for (let i = 0; i < 5; i++){
+    console.log ("Imprindo de 0 a 4 !", i);
+}
+
+for (let i = 1; i <= 5; i++){
+    console.log ("Imprimindo de 1 a 5 !", i); 
+}
+
+for (let i = 0; i <= 5; i++){
+     if (i % 2 !== 0){
+         console.log ("Imprimindo números ímpares !", i);
+     }
+}
+
+for (let i = 0; i <= 5; i++){
+    if (i % 2 == 0){
+        console.log ("Imprimindo números pares !", i);
+    }
+    
+}
