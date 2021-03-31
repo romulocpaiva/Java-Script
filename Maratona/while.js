59a07b41064590bd2069f6e89b0a6f32ce589a9f
romulocpaiva <64547945+romulocpaiva@users.noreply.github.com>
2021-03-31 17:44:25 -0300

    while

---
+++ Maratona/while.js
@@ -0,0 +1,75 @@
+let a = 0;
+while (a < 5){
+    console.log("Imprimindo de 0 a 4 !", a);
+    a ++;
+}
+
+let b = 1;
+while (b <=5){
+    console.log("Imprimindo de 1 a 5 !", b);
+    b ++;
+}
+
+let c = 0;
+while (c <= 5){
+    if (c % 2 !== 0){
+        console.log("Imprimindo números ímpares !", c);
+    }
+    c ++;
+}
+
+let d = 0;
+while (d <= 5){
+    if (d % 2 == 0){
+        console.log("Imprimindo números pares !", d);        
+    }
+    d ++;
+}
+
+let e = 4;
+while (e >= 0){
+    console.log("Imprimindo de 4 a 0 !", e);
+    e --;
+}
+
+
+let f = 5;
+while (f >= 0){
+    if (f % 2 !== 0){
+        console.log("Imprimindo números ímpares decrescente !", f);
+    }
+    f --;
+}
+
+let g = 5;
+while (g >= 0){
+    if (g % 2 == 0){
+        console.log("Imprimindo números pares decrescente !", g);
+    }
+    g --;
+}
+
+
+
+
+
+
+// let i = 5;
+// while (i >= 1){
+//     if (i % 2 !== 0){
+//         console.log(i);
+//     }
+//     i--;
+// }
+
+
+
+
+
+// while (i >= 1){
+//     if (i % 2 !== 0){
+//         console.log(i);
+//     }
+//     i--;
+
+// }
