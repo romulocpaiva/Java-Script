f8751ce7b706b04ef1fb24e87e789e585640d743
romulocpaiva <64547945+romulocpaiva@users.noreply.github.com>
2021-04-01 23:24:41 -0300

    fizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,20 @@
+// Divisivel por 3 => Fizz
+// Divisivel por 5 => Buzz
+// Divisivel por 3 e 5 => FizzBuzz
+// Não é divisivel por 3 ou 5 => entrada
+// Não é um número = Não é um número
+
+const resultado = fizzBuzz(10);
+console.log (resultado);
+
+function fizzBuzz(entrada){
+    if (typeof entrada !== 'number')
+        return 'Não é um número';
+    if ((entrada %3 === 0) && (entrada %5 === 0))
+        return 'FizzBuzz';
+    if (entrada %3 === 0)
+        return 'Fizz';
+    if (entrada %5 === 0)
+        return 'Buzz';   
+
+}
