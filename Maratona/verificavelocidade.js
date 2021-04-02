956b903dd61f64081531567cc77fe4e6491d0356
romulocpaiva <64547945+romulocpaiva@users.noreply.github.com>
2021-04-02 00:44:51 -0300

    verifica velocidade

---
+++ Maratona/verificavelocidade.js
@@ -0,0 +1,23 @@
+// velocidade máxima de até 70
+// a cada 5 acima do limite você ganha 1 ponto
+// Math.Floor()
+//caso pontos maior que 12 -> "carteira suspensa"
+
+let permitida = 70;
+let kmh = verificaVelocidade(84);
+
+
+function verificaVelocidade(velocidade){
+    if (velocidade <= permitida)
+        console.log (velocidade + ' KM/h está dentro da velocidade permitida');
+    else {
+        const pontos = Math.floor(((velocidade - permitida) /5 ));
+        console.log(velocidade - permitida + ' km/h acima da velocidade permitida');
+        if (pontos >= 12)
+            console.log (pontos + ' Pontos, CNH suspensa');
+            else
+                console.log (pontos + ' Pontos, CNH regular');
+
+    }
+}
+
