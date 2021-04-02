410ed67b99f941278114b2bd81d8bc641cb92007
romulocpaiva <64547945+romulocpaiva@users.noreply.github.com>
2021-04-01 21:11:43 -0300

    for of

---
+++ Maratona/for of.js
@@ -0,0 +1,18 @@
+// for
+// while
+// do while
+
+// for-of
+
+const cores = ['vermelho', 'azul', 'verde', 'amarelo'];
+
+for (let cor of cores) {
+    console.log(cor);
+}
+
+
+const pneus = ['Michelin', 'Bridgestone', 'Pirelli', 'Goodyear', 'Yokohama', 'Dunlop'];
+
+for (let empresas of pneus){
+    console.log(empresas);
+}
